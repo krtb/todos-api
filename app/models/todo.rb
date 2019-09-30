7ba2c3875294e640c0426f92a3584e512235636b
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-30 16:39:47 -0400

    validate presence of title and created by table attributes

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,4 +2,6 @@ class Todo < ApplicationRecord
     # rails g model Todo title:string created_by:string
     has_many :items, dependent: :destroy # create model asscociations
 
+    # validations
+    validates_presence_of :title, :created_by
 end
