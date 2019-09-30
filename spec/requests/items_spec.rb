23fb871b717e6eca7ba3423dcf6c8dc99764c9f5
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-30 17:05:43 -0400

    create factory for Items model

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -1,1 +1,8 @@
 # not writing controller specs, writing REQUEST SPECS INSTEAD
+FactoryBot.define do
+  factory :item do
+    name { Faker::StarWars.character }
+    done false
+    todo_id nil
+  end
+end
