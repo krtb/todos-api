98fc158f9e4c3a0bb6eb5f68e3c391118768b2b3
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-30 15:06:22 -0400

    # start the transaction strategy as examples are run

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -73,9 +73,17 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
+  # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
     DatabaseCleaner.strategy = :transaction
   end
+
+  # start the transaction strategy as examples are run
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+  
 end
