dc60c59e9a0d8188530fbcc193867e005bec8f5d
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-30 17:05:08 -0400

    define factory for todos model

--- spec/factories/todos.rb
+++ spec/factories/todos.rb
@@ -1,1 +1,11 @@
 # will contain model factories which will provide the test data
+FactoryBot.define do
+  factory :todo do
+    title { Faker::Lorem.word }
+    created_by { Faker::Number.number(10) }
+  end
+end
+
+# wrapping faker methods in a block
+# ensures that faker generates dynamic data each time
+# factory is invoked
