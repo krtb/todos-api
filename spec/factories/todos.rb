d5f0d758e1e89481b31c980e5c6afb3170afaf8d
Kurt Bauer <kurtbauer55@protonmail.com>
2019-10-01 09:21:07 -0400

    change from positional arg to keyword arg

--- spec/factories/todos.rb
+++ spec/factories/todos.rb
@@ -2,7 +2,7 @@
 FactoryBot.define do
   factory :todo do
     title { Faker::Lorem.word }
-    created_by { Faker::Number.number(10) }
+    created_by { Faker::Number.number(digits: 10) }
   end
 end
 
