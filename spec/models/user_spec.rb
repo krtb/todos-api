7b3e28742e08960a579f2bb374d019f39ddbfd20
Kurt Bauer <kurtbauer55@protonmail.com>
2019-10-04 19:10:30 -0400

    define User model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
+# Test suite for User model
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  # ensure User model has a 1:m relationship with the Todo model
+  it { should have_many(:todos) }
+  # Validation tests
+  # ensure name, email and password_digest are present before save
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password_digest) }
 end
+
+# USER: should be able to manage their own todo lists
+# reasong for having a 1:many relationship
+# VALIDATE: on user account creation, have required user information
