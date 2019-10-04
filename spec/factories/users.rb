e1a731bc3e1a5bd9799f45301e36b09929c43733
Kurt Bauer <kurtbauer55@protonmail.com>
2019-10-04 19:12:28 -0400

    add user factory to create test users for test suite

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+# will be used by test suite to create test users
+FactoryBot.define do
+  factory :user do
+    name { Faker::Name.name }
+    email 'foo@bar.com'
+    password 'foobar'
+  end
+end
