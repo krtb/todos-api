630385c934f02375c907d96afb027ce944391ee7
Kurt Bauer <kurtbauer55@protonmail.com>
2019-10-09 12:28:17 -0400

    define controllers in V1 namespace

--- app/controllers/v1/todos_controller.rb
+++ app/controllers/v1/todos_controller.rb
@@ -1,45 +1,47 @@
-class TodosController < ApplicationController
-  before_action :set_todo, only: [:show, :update, :destroy]
+module V1
+  class TodosController < ApplicationController
+    before_action :set_todo, only: [:show, :update, :destroy]
 
-  # GET /todos
-  def index
-    # get current user todos
-    @todos = current_user.todos
-    json_response(@todos)
+    # GET /todos
+    def index
+      # get current user todos
+      @todos = current_user.todos
+      json_response(@todos)
+    end
+
+    # POST /todos
+    def create
+      # create todos belonging to current user
+      @todo = current_user.todos.create!(todo_params)#by using (create!), model will raise an exception
+      json_response(@todo, :created)
+    end
+
+    # GET /todos/:id
+    def show
+      json_response(@todo)
+    end
+
+    # PUT /todos/:id
+    def update
+      @todo.update(todo_params)
+      head :no_content
+    end
+
+    # DELETE /todos/:id
+    def destroy
+      @todo.destroy
+      head :no_content
+    end
+
+    private
+
+    def todo_params
+      # whitelist params
+      params.permit(:title)  
+    end
+
+    def set_todo
+      @todo = Todo.find(params[:id])
+    end
   end
-
-  # POST /todos
-  def create
-    # create todos belonging to current user
-    @todo = current_user.todos.create!(todo_params)#by using (create!), model will raise an exception
-    json_response(@todo, :created)
-  end
-
-  # GET /todos/:id
-  def show
-    json_response(@todo)
-  end
-
-  # PUT /todos/:id
-  def update
-    @todo.update(todo_params)
-    head :no_content
-  end
-
-  # DELETE /todos/:id
-  def destroy
-    @todo.destroy
-    head :no_content
-  end
-
-  private
-
-  def todo_params
-    # whitelist params
-    params.permit(:title)  
-  end
-
-  def set_todo
-    @todo = Todo.find(params[:id])
-  end
-end
+end # end of defining controllers in v1 namespace
