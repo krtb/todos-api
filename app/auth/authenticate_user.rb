ed22c1d4e2e0fecf634be9fa0700421a8f8bd717
Kurt Bauer <kurtbauer55@protonmail.com>
2019-10-09 10:42:25 -0400

    define auth_user class, checks for email and password

--- app/auth/authenticate_user.rb
+++ app/auth/authenticate_user.rb
@@ -1,1 +1,28 @@
 # this class will be responsible for authenticating users via (email/password)
+class AuthenticateUser
+  def initialize(email, password)
+    @email = email
+    @password = password
+  end
+
+  # Service entry point
+  def call
+    JsonWebToken.encode(user_id: user.id) if user
+  end
+
+  private
+
+  attr_reader :email, :password
+
+  # verify user credentials
+  def user
+    user = User.find_by(email: email)
+    return user if user && user.authenticate(password)
+    # raise Authentication error if credentials are invalid
+    raise(ExceptionHandler::AuthenticationError, Message.invalid_credentials)
+  end
+end
+
+# service accepts: user email and password
+# checks if they're valid
+# then returns a token
