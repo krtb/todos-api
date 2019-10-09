d74ae399e3f90ccff96d15746a1e7a53896f0fe8
Kurt Bauer <kurtbauer55@protonmail.com>
2019-10-09 10:56:52 -0400

    create user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+  # POST /signup
+  # return authenticated token upon signup
+  def create
+    user = User.create!(user_params)
+    auth_token = AuthenticateUser.new(user.email, user.password).call
+    response = { message: Message.account_created, auth_token: auth_token }
+    json_response(response, :created)
+  end
+
+  private
+
+  def user_params
+    params.permit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
 end
+
+# user_controller: attempts to create a users, returns a JSON response with result
+# user ActiveRecord's (create!) method, to that if error, exception will be raised
+# and handled in the exception handler
