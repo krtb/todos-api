b0e09587d731b80221a9d1a42016b5c6edb7bc1b
Kurt Bauer <kurtbauer55@protonmail.com>
2019-10-09 11:49:34 -0400

    modify todos to know about user

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -1,5 +1,6 @@
 # not writing controller specs, writing REQUEST SPECS INSTEAD
 require 'rails_helper'
+
 RSpec.describe 'Items API' do
   # Initialize the test data
   let(:user) { create(:user) }
@@ -12,7 +13,7 @@ RSpec.describe 'Items API' do
   # Test suite for GET /todos/:todo_id/items
   describe 'GET /todos/:todo_id/items' do
     before { get "/todos/#{todo_id}/items", params: {}, headers: headers }
-    
+
     context 'when todo exists' do
       it 'returns status code 200' do
         expect(response).to have_http_status(200)
@@ -38,7 +39,7 @@ RSpec.describe 'Items API' do
 
   # Test suite for GET /todos/:todo_id/items/:id
   describe 'GET /todos/:todo_id/items/:id' do
-    before { get "/todos/#{todo_id}/items/#{id}" }
+    before { get "/todos/#{todo_id}/items/#{id}", params: {}, headers: headers }
 
     context 'when todo item exists' do
       it 'returns status code 200' do
@@ -72,6 +73,11 @@ RSpec.describe 'Items API' do
         post "/todos/#{todo_id}/items", params: valid_attributes, headers: headers
       end
 
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
     context 'when an invalid request' do
       before { post "/todos/#{todo_id}/items", params: {}, headers: headers }
 
@@ -104,7 +110,7 @@ RSpec.describe 'Items API' do
       end
     end
 
-    context 'when the item does not exist' do
+    context 'when item does not exist' do
       let(:id) { 0 }
 
       it 'returns status code 404' do
