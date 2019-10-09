1f5b4c5dc4b9dc6b306af659203739aba0c2b2ac
Kurt Bauer <kurtbauer55@protonmail.com>
2019-10-09 11:03:26 -0400

    define application_controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::API
-    include Response #make application aware of helper modules, that will handle errors
-    include ExceptionHandler
+  include Response
+  include ExceptionHandler
+
+  # called before every action on controllers
+  before_action :authorize_request
+  attr_reader :current_user
+
+  private
+
+  # Check for valid request token and return user
+  def authorize_request
+    @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+  end
 end
+
+# on every request: app will verify request, by calling the (request authorization service)
+# IF authed, will set (CURRENT_USER ) object to be used in other controllers
+# don't have guard classes, or conditionals, because of our error handling implementation
