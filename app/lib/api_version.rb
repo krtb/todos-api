8d6368023a6982de00633232d9c9e05986c4093f
Kurt Bauer <kurtbauer55@protonmail.com>
2019-10-09 12:10:08 -0400

    add class for api versioning with content negoatiation

--- app/lib/api_version.rb
+++ app/lib/api_version.rb
@@ -1,2 +1,25 @@
 # class checks API version from request headers, routes to appropriate header module. 
 # lives here because not domain specific
+class ApiVersion
+  attr_reader :version, :default 
+
+  def initialize(version, default = false)# class accepts a version and default flag on initialization
+    @version = version
+    @default = default
+  end
+
+  # check whether version is specified or is default
+  def matches?(request) # implement instance method, called with request object upon initialization
+    check_headers(request.headers) || default
+  end
+
+  private
+
+  # BELOW PROCESS IS CALLED: CONTENT NEGOTIATION
+  def check_headers(headers)
+    # check version from Accept headers; expect custom media type `todos`
+    accept = headers[:accept] # from request object, can access the Accept headers
+    accept && accept.include?("application/vnd.todos.#{version}+json") # check for the requested version or if default version
+  end
+end
+
